d3808c02e5e395ef4d1c735c9acb4d12a556cf35
Jawaad Mahmood <ideas@jawaadmahmood.com>
2023-01-03 20:15:55 -0700

    Remove Hardcoded values / Magic Numbers / Add Config Options (#3)
    * Added example config and config settings to the main file.
    * Fixed README.md file
    * Fix crash if the front button is clicked.
    * Refactor magic values and add in types
    * There's no need to use the x0/x1/etc... values anymore.
    * Move Threshold / Samples to the config file
    * Ignore IntelliJ Files

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
 import statistics
 from pprint import pprint
 import configparser
+from typing import TypedDict, List
 
 import evdev
 
-TOP_LEFT = 'x1'
-TOP_RIGHT = 'x0'
-BOTTOM_LEFT = 'y1'
-BOTTOM_RIGHT = 'y0'
-SAMPLES_TO_USE = 10
-THRESHOLD = 0
+TOP_RIGHT_CODE = evdev.ecodes.ABS_HAT0X
+TOP_LEFT_CODE = evdev.ecodes.ABS_HAT1X
+BOTTOM_LEFT_CODE = evdev.ecodes.ABS_HAT0Y
+BOTTOM_RIGHT_CODE = evdev.ecodes.ABS_HAT1Y
 
 
-def total(input_data):
-    return input_data[TOP_LEFT] + input_data[TOP_RIGHT] + input_data[BOTTOM_LEFT] + input_data[BOTTOM_RIGHT]
+class WeightData(TypedDict):
+    TOP_LEFT: int
+    TOP_RIGHT: int
+    BOTTOM_LEFT: int
+    BOTTOM_RIGHT: int
 
 
-def calculate_weight(device: evdev.InputDevice):
-    data = {
-        "x1": 0,
-        "x0": 0,
-        "y1": 0,
-        "y0": 0,
-    }
+class Response(TypedDict):
+    max: int
+    grouped_median: float
+
+
+def total(input_data: WeightData) -> int:
+    return input_data['TOP_LEFT'] + input_data['TOP_RIGHT'] + input_data['BOTTOM_LEFT'] + input_data['BOTTOM_RIGHT']
+
+
+def calculate_weight(device: evdev.InputDevice, threshold=0, samples_to_use=10) -> Response:
+    data: WeightData = {'TOP_LEFT': 0, 'TOP_RIGHT': 0, 'BOTTOM_LEFT': 0, 'BOTTOM_RIGHT': 0}
+
     max_weight = 0
-    event_data = []
+    event_data: List[int] = []
 
     for event in device.read_loop():
-        if event.code == evdev.ecodes.ABS_HAT0X:
-            data[TOP_RIGHT] = event.value
-        elif event.code == evdev.ecodes.ABS_HAT1X:
-            data[TOP_LEFT] = event.value
-        elif event.code == evdev.ecodes.ABS_HAT0Y:
-            data[BOTTOM_RIGHT] = event.value
-        elif event.code == evdev.ecodes.ABS_HAT1Y:
-            data[BOTTOM_LEFT] = event.value
+        if event.code == TOP_RIGHT_CODE:
+            data['TOP_RIGHT'] = event.value
+        elif event.code == TOP_LEFT_CODE:
+            data['TOP_LEFT'] = event.value
+        elif event.code == BOTTOM_LEFT_CODE:
+            data['BOTTOM_LEFT'] = event.value
+        elif event.code == BOTTOM_RIGHT_CODE:
+            data['BOTTOM_RIGHT'] = event.value
         elif event.code == evdev.ecodes.BTN_A:
             # This only happens when you are hitting the power button in the front
             pass
@@ -47,12 +54,11 @@ def calculate_weight(device: evdev.InputDevice):
             if running_total > max_weight:
                 max_weight = running_total
 
-            if running_total <= THRESHOLD:  # Someone stepped off the balance board.
-                pprint({
+            if running_total <= threshold:  # Someone stepped off the balance board.
+                return {
                     'max': max_weight,
-                    'grouped_median': statistics.median_grouped(event_data, SAMPLES_TO_USE)
-                })
-                event_data = []
+                    'grouped_median': statistics.median_grouped(event_data, samples_to_use)
+                }
         else:
             raise IOError(f'Unexpected event {evdev.categorize(event)}')
 
@@ -60,5 +66,11 @@ def calculate_weight(device: evdev.InputDevice):
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.ini')
-    balance_board: evdev.InputDevice = evdev.InputDevice(config['DEFAULT']['BalanceBoardDeviceLocation'])
-    calculate_weight(balance_board)
+    balance_board: evdev.InputDevice = evdev.InputDevice(
+        config['DEFAULT']['BalanceBoardDeviceLocation'],
+    )
+    pprint(calculate_weight(
+        balance_board,
+        int(config['DEFAULT']['Threshold']),
+        int(config['DEFAULT']['SamplesToUse']),
+    ))
