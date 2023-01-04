eb6fd305db6c39e296fc0777f5db80af37464839
Jawaad Mahmood <ideas@jawaadmahmood.com>
2023-01-03 18:48:36 -0700

    Added example config and config settings to the main file. (#1)
    * Added example config and config settings to the main file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import statistics
 from pprint import pprint
+import configparser
 
 import evdev
 
@@ -8,14 +9,14 @@ TOP_RIGHT = 'x0'
 BOTTOM_LEFT = 'y1'
 BOTTOM_RIGHT = 'y0'
 SAMPLES_TO_USE = 10
-BALANCE_BOARD_DEVICE_LOCATION = '/dev/input/event27'
+THRESHOLD = 0
 
 
 def total(input_data):
     return input_data[TOP_LEFT] + input_data[TOP_RIGHT] + input_data[BOTTOM_LEFT] + input_data[BOTTOM_RIGHT]
 
 
-def calculateWeight(device: evdev.InputDevice):
+def calculate_weight(device: evdev.InputDevice):
     data = {
         "x1": 0,
         "x0": 0,
@@ -35,7 +36,6 @@ def calculateWeight(device: evdev.InputDevice):
         elif event.code == evdev.ecodes.ABS_HAT1Y:
             data[BOTTOM_LEFT] = event.value
         elif event.code == evdev.ecodes.SYN_REPORT and event.value == 3:
-            # disregard.
             pass
         elif event.code == evdev.ecodes.SYN_REPORT and event.value == 0:
             # We've gotten the full stream and can now calculate the outcome.
@@ -44,7 +44,7 @@ def calculateWeight(device: evdev.InputDevice):
             if running_total > max_weight:
                 max_weight = running_total
 
-            if running_total == 0:  # Someone stepped off the balance board.
+            if running_total <= THRESHOLD:  # Someone stepped off the balance board.
                 pprint({
                     'max': max_weight,
                     'grouped_median': statistics.median_grouped(event_data, SAMPLES_TO_USE)
@@ -55,5 +55,7 @@ def calculateWeight(device: evdev.InputDevice):
 
 
 if __name__ == '__main__':
-    balance_board: evdev.InputDevice = evdev.InputDevice(BALANCE_BOARD_DEVICE_LOCATION)
-    calculateWeight(balance_board)
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    balance_board: evdev.InputDevice = evdev.InputDevice(config['DEFAULT']['BalanceBoardDeviceLocation'])
+    calculate_weight(balance_board)
