c9ba254e33d4bf90eb028252bd4976198935dcf8
Jawaad Mahmood <jawaad.mahmood@avaxia-group.com>
2023-01-04 20:59:48 -0700

    Altered structure to work w/ pip.

---
+++ setup.py
@@ -0,0 +1,11 @@
+__author__ = 'jawaad'
+
+from distutils.core import setup
+
+setup(name='bbev',
+      version='0.0.1',
+      description='Using the Wii Balance Board with Linux\'s EVDev interface.',
+      author='Jawaad Mahmood',
+      author_email='ideas@jawaadmahmood.com',
+      url='https://github.com/jmahmood/bbev/',
+      packages=['bbev'], requires=['evdev'])
